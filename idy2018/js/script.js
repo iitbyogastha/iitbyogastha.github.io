d18a267d4e44b0a6e936c73f62eac471c64cdbc6
Sourabh Bhat <heySourabh@gmail.com>
2018-05-26 04:24:41 +0530

    Updated Yogathon page

--- idy2018/js/script.js
+++ idy2018/js/script.js
@@ -3,6 +3,7 @@ $(function() {
     $(this).siblings('.panel-heading').addClass('active');
   })
 });
+
 $(function() {
   $('.collapse').on('hide.bs.collapse', function () {
     $(this).siblings('.panel-heading').removeClass('active');
