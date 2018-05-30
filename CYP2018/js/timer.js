be9c7fc09fdeb53be17f8b94882130e099bac16c
Sourabh Bhat <heySourabh@gmail.com>
2018-05-30 10:53:58 +0530

    Added modal for registration button.

---
+++ CYP2018/js/timer.js
@@ -0,0 +1,30 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 21, 2018 06:45:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("cyp-date").innerHTML = "Time Left: " + days + " days, " + hours + "h "
+    + minutes + "m " + seconds + "s";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("cyp-date").innerHTML = "CYP EVENT IS OVER";
+    }
+
+    document.getElementById("cyp-date").className = "timer";
+}, 1000);
