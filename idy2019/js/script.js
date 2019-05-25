75b823cd26fef0725162274d5237cdd8cc778d43
yopradeep <himirikapradeep@gmail.com>
2019-05-25 21:25:29 +0530

    some interface  changes

---
+++ idy2019/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  })
+});
+
+$(function() {
+  $('.collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  })
+});
