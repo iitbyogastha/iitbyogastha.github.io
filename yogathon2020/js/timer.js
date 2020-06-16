661490b4d2f874e805004fb373eb04023076097c
Narayana-Rao <50410669+Narayana-Rao@users.noreply.github.com>
2020-06-16 01:50:08 -0700

    Update Yogathin 2020

--- yogathon2020/js/timer.js
+++ yogathon2020/js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 21, 2019 17:30:00").getTime();
+var countDownDate = new Date("Jun 21, 2020 17:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
