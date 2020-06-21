e48469e53a37156dcce7c4634d2d48cc9a0f25b2
Narayana-Rao <50410669+Narayana-Rao@users.noreply.github.com>
2020-06-21 06:28:38 +0530

    Update live countdown

--- live/countdown/js/main.js
+++ live/countdown/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "June 21, 2019 06:30:00",
+	date: "June 21, 2020 06:30:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
