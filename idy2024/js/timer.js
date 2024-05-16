c365e8a5384c9df04950b28d9e847655e4445773
Sakshi Agarwal <20D110022@iitb.ac.in>
2024-05-16 19:36:20 +0530

    changes

--- idy2024/js/timer.js
+++ idy2024/js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 21, 2023 7:00:00").getTime();
+var countDownDate = new Date("Jun 21, 2024 7:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
